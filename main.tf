6fc68cc7024735f4eacbc3471f4b6807605e6367
Aswanidevm <maswanidev@gmail.com>
2023-08-17 08:19:34 +0530

    ad

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ resource "aws_subnet" "main" {
   web_subnet_cidr_block = element(var.web_subnet_cidr_block, count.index)
 
   tags = {
-    Name = "Main"
+    tags = merge ({ Name = "${var.env}-web_subnet"}, var.tags )
   }
 }
