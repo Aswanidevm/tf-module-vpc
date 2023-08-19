26bd309c51730db96fca360e98b577dd066af444
Aswanidevm <maswanidev@gmail.com>
2023-08-19 07:48:49 +0530

    wsdf

--- main.tf
+++ main.tf
@@ -21,3 +21,22 @@ module "subnets" {
   az = var.az
 
 }
+
+module "single_account_single_region" {
+  source = "./subnets"
+
+  # providers = {
+  #   aws.this = aws
+  #   aws.peer = aws
+  # }
+
+  this_vpc_id = var.this_vpc_id
+  peer_vpc_id = var.default_vpc_id
+
+  auto_accept_peering = true
+
+  tags = {
+    Name        = "tf-single-account-single-region"
+    Environment = "Test"
+  }
+}
