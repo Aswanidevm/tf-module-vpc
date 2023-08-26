fc7879333dab58640ca730782a8c3a81b3923a1a
Aswanidevm <maswanidev@gmail.com>
2023-08-26 10:20:17 +0530

    asd

--- subnets/main.tf
+++ subnets/main.tf
@@ -8,3 +8,19 @@ resource "aws_subnet" "main" {
     tags = merge ({ Name = "${var.env}-${var.subnet_name}-web_subnet"}, var.tags )
   
 }
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  # route {
+  #   cidr_block = "10.0.1.0/24"
+  #   gateway_id = aws_internet_gateway.example.id
+  # }
+
+  # route {
+  #   ipv6_cidr_block        = "::/0"
+  #   egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  # }
+
+  tags = merge ({ Name = "${var.env}-${var.subnet_name}-web_subnet"}, var.tags )
+}
